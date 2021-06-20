284accba84ad9339ad77ee847a5bce79963c13dc
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2021-06-20 17:01:45 +0530

    Quadratic Eq Solutions

--- Others/Quadratic_equaition_solutions.py
+++ Others/Quadratic_equaition_solutions.py
@@ -4,9 +4,9 @@ a = int(input())
 b = int(input())
 c = int(input())
 
-if (b**2 - (4*a*c))>=0:
-    d = b**2 - 4*a*c
-    
-    print("Roots of the given equation are: ")
-    print("(",0-b,"+ root(",d,") )","/",2*a)
-    print("(",0-b,"- root(",d,") )","/",2*a)
+# if (b**2 - (4*a*c))>=0:
+d = b**2 - 4*a*c
+
+print("Roots of the given equation are: ")
+print("[ ",0-b,"+ root(",d,") ]","/",2*a)
+print("[ ",0-b,"- root(",d,") ]","/",2*a)
