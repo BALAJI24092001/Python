098b31dc0f68473e61b4f88aafe712cacc2f8ed1
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2021-06-21 11:52:08 +0530

    Matrix mul

--- Others/Matrix_multiplication_BET.py
+++ Others/Matrix_multiplication_BET.py
@@ -1,7 +1,9 @@
 from numpy import *
 a = int(input("Enter matrix length: "))
-arr1 = array([[0*a]*a])
-arr2 = array([[0*a]*a])
+arr1 = zeros((a,a))
+arr2 = zeros((a,a))
+
+
 for i in range(a):
     for j in range(a):
         print(arr1[i][j], end=" ")
@@ -10,29 +12,39 @@ for i in range(a):
     for j in range(a):
         print(arr2[i][j], end=" ")
     print();
+
+
 print("Enter elements of first array: ")
-for i in range(0,a):
-    for j in range(0,a):
+for i in range(a):
+    for j in range(a):
         arr1[i][j] = int(input())
 print("Enter elements of second array: ")
-for i in range(0,a):
-    for j in range(0,a):
+for i in range(a):
+    for j in range(a):
         arr2[i][j] = int(input())
 
-solArr = array([[0*a]*a])
+
+for i in range(a):
+    for j in range(a):
+        print(arr1[i][j], end=" ")
+    print();
+for i in range(a):
+    for j in range(a):
+        print(arr2[i][j], end=" ")
+    print();
+
+
+solArr = zeros((a,a))
 c = 0;
 d = 0;
-for i in range(0, a*a):
+for i in range(a*a):
     temp = 0;
-    for j in range(0,a):
-        for l in range(0, a):
+    for j in range(a):
+        for l in range(a):
             temp += arr1[j][l] * arr2[l][j]
-        solArr[c][d];
-        if c == a:
-            c = 0;
-        else:
-            c+=1;
+        solArr[c][d]= temp;
         if d == a:
+            c+=1;
             d = 0;
         else:
             d+=1;
