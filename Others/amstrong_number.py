ad426882e789c9131aeca3b971d0d8f91531480b
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2021-07-17 22:49:50 +0530

    17-07-2021

--- Others/amstrong_number.py
+++
@@ -1,5 +0,0 @@
-a = int(input("Enter a number: "))
-b = 0
-temp =a
-while b != 0:
-    temp = te
---
+++ test.py
@@ -0,0 +1,4 @@
+for i in range(5):
+    print("  "*(5-i), "* "*(i+1))
+for i in range(4):
+    print("  "*(i+2), "* "*(4-i))
