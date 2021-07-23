8766f94d0e4439b09d23673c0b42276c452be57d
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2021-07-23 18:26:23 +0530

    23-07-2021

--- Others/age_calculator.py
+++ Others/age_calculator.py
@@ -1,4 +1,22 @@
-from numpy import *
-a = int(input("Enter year of birth: "))
-b = int(input("Enter present year: "))
-print("Your age is: ", b-a)
+from datetime import datetime
+print("Enter date of birth: ")
+day = int(input("Day: "))
+month = int(input("Month: "))
+year = int(input("Year: "))
+now = datetime.now()
+pday = now.day
+pmonth = now.month
+pyear = now.year
+print(pyear)
+age = pyear - year
+print(age)
+if(month <= pmonth):
+    if(month == pmonth):
+        if(day < pday):
+            print("Age: ", age-1)
+        else:
+            print("Age: ", age)
+    else:
+        print("Age: ", age)
+else:
+    print("Age: ", age-1)
