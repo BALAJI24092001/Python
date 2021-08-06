71c8a00dc3dd686b3c4156553732a99f59b691fb
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2021-08-06 10:37:57 +0530

    06-08-2021

---
+++ SQLite/database.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+# con = sqlite3.connect(":memory:") #To create a connection in the memory
+con = sqlite3.connect("customers.db")
+
+# Create a cursor
+c = con.cursor()
+# create a table
+c.execute(""" CREATE TABLE custmers(
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT
+) 
+""")
+
+
+# commit our command
+con.commit()
+
+# colse our connection
+con.close()
--- SQLite/test.py
+++
@@ -1,4 +0,0 @@
-import sqlite3
-
-# con = sqlite3.connect(":memory:") #To create a connection in the memory
-con = sqlite3.connect("customers")
