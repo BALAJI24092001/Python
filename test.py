6f49d4be81dc59f9bb83b09da46523af6890fabd
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2021-08-08 20:51:54 +0530

    08-08-2021

--- test.py
+++ test.py
@@ -1,16 +1,30 @@
-for i in range(5):
-    print("  "*(5-i), "* "*(i+1))
-for i in range(4):
-    print("  "*(i+2), "* "*(4-i))
+class Node:
+    def __init__(self, datavalue) -> None:
+        self.datavalue = datavalue
+        self.nextvalue = None
 
-    
-# output:
-#            * 
-#          * * 
-#        * * * 
-#      * * * * 
-#    * * * * * 
-#      * * * * 
-#        * * * 
-#          * * 
-#            * 
+
+class SLinkedList:
+    def __init__(self) -> None:
+        self.headval = None
+
+    def AtEnd(self, datavalue):
+        NewNode = Node(datavalue)
+        if self.headval is None:
+            self.headvalue = NewNode
+            return
+        laste = self.headval
+        while laste.nextvalue:
+            laste = laste.nextvalue
+        laste.nextvalue = NewNode
+
+    def AtBeg(self, newdata):
+        newnode = Node(newdata)
+        newnode.nextvalue = self.headval
+        self.headval = newnode
+
+    def listprint(self):
+        laste = self.headvalue
+        while laste is not None:
+            print(laste.datavalue)
+            laste = laste.nextvalue
