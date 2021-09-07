18d3de0270cf31d769ad7b478d2d82a0288e6838
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2021-09-07 23:42:45 +0530

    pyenv

---
+++ Practice/friendsTaste.py
@@ -0,0 +1,18 @@
+def nBinVal(x):
+
+    next = 0
+    if(x):
+        rightOne = x & -(x)
+        nextHigherOneBit = x + int(rightOne)
+        rightOnesPattern = x ^ int(nextHigherOneBit)
+        rightOnesPattern = (int(rightOnesPattern) /
+                            int(rightOne))
+        rightOnesPattern = int(rightOnesPattern) >> 2
+        next = nextHigherOneBit | rightOnesPattern
+    return next
+
+
+n = int(input())
+for i in range(n):
+    x = int(input())
+    print(nBinVal(x))
