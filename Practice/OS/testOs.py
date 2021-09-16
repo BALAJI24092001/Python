4838d77da47dba90351e94ade00426db37df542d
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2021-09-16 22:31:03 +0530

    16-09-2021

---
+++ Practice/OS/testOs.py
@@ -0,0 +1,12 @@
+import os
+print(os.getcwd())
+os.chdir('../')
+print(os.getcwd())
+os.chdir('./GitHub')
+print(os.getcwd())
+os.chdir('./Python')
+print(os.getcwd())
+
+
+# os.getcwd()  --  current directory
+# os.chdir()  -- change directory
