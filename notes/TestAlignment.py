20a6e6ceabc2f800a7726f957776ec0b019fdb88
BALAJI <dbalajivaraprasad@gmail.com>
2021-11-22 21:17:17 +0530

    text alignment, spliting and

---
+++ notes/TestAlignment.py
@@ -0,0 +1,22 @@
+# %%
+width = 20
+print ('HackerRank'.ljust(width,'-'))
+
+# %%
+width = 20
+print ('HackerRank'.center(width,'-'))
+
+# %%
+width = 20
+print ('HackerRank'.rjust(width,'-'))
+
+# %%
+a = "this is a string"
+a = a.split(" ") # a is converted to a list of strings. 
+print(a)
+
+# %%
+a = " ".join(a)
+print (a)
+
+
